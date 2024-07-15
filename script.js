50f18ac347637c44aefe6eb4920250a8e6e73894
Ayaan Grover <ayaangrover@gmail.com>
2024-07-15 16:39:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -70,3 +70,40 @@ window.sendMessage = async function() {
     alert('You must be signed in to send a message');
     return;
   }
+
+  let messageInput = document.getElementById('message-input');
+  let message = encodeURIComponent(messageInput.value);
+  let name = encodeURIComponent(googleUserName); // Use the Google user's name
+  let time = Date();
+  let fullUrl = url + 'send/' + message + ' (' + name + ', ' + time + ')';
+  console.log(fullUrl)
+  console.log('sent on ' + time)
+  await fetch(fullUrl);
+}
+
+window.signOutUser = function() {
+  auth.signOut()
+    .then(() => {
+      // Sign-out successful.
+      googleUserName = '';
+      // nameInput.style.display = 'name-input';
+    })
+    .catch((error) => {
+      // An error happened.
+      console.error('Error signing out: ', error);
+    });
+}
+
+async function getMessages() {
+  let res = await fetch(url);
+  let messages = await res.json();
+
+  let messagesDiv = document.getElementById('message-div');
+  messagesDiv.innerHTML = '';
+
+  for (let i = messages.length - 1; i >= 0; i--) {
+    messagesDiv.innerHTML += '<p>' + messages[i] + '</p>';
+  }
+}
+
+setInterval(getMessages, 1000);
